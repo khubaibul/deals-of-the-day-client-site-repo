b644b542ea0077de2e0ae151ae2baaa4cccb4303
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-11-26 14:06:46 +0600

    Product Removed From Advertise After Successful Payment

--- src/Components/Pages/Home/Advertise/Advertise.js
+++ src/Components/Pages/Home/Advertise/Advertise.js
@@ -47,7 +47,7 @@ const Advertise = () => {
 
     };
 
-    if (advertisementProducts) {
+    if (advertisementProducts.length) {
         return <div className='my-48'>
             <h1 className="mb-5 text-3xl font-bold text-neutral text-center">ADVERTISED PRODUCT</h1>
             <Slider {...settings}>
